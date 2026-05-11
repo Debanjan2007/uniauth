fd642287eff07597b2efb9c3b93eab1d9fb17fa3
Debanjan Das <microsoft2420@gmail.com>
2026-05-11 21:20:14 +0530

    Add code challenge and verifier with crypto pkg

---
+++ src/pkce/generateCodeChallenge.ts
@@ -0,0 +1,7 @@
+﻿// @ts-ignore
+import crypto from 'node:crypto'
+
+export async function generateCodeChallenge(code_verifier: string) {
+    const code_challenge = crypto.createHash('sha256').update(code_verifier).digest('base64url')
+    return code_challenge
+}
---
+++ src/pkce/generateCodeVerifier.ts
@@ -0,0 +1,6 @@
+﻿// @ts-ignore
+import crypto from 'node:crypto';
+
+export function generateCodeVerifier() {
+    return crypto.randomBytes(32).toString('base64url')
+}
