7257d31b6c706cdc3f5344717d29c4eb323ee87e
Debanjan Das <microsoft2420@gmail.com>
2026-05-13 00:03:25 +0530

    Add Baseclass to core

---
+++ src/providers/core/BaseClass.ts
@@ -0,0 +1,8 @@
+﻿import type { TokenResponse } from "./types/TokenResponse.types";
+import type { UserProfile } from "./types/UserProfile.types";
+
+abstract class BaseProviderClass{
+    abstract getAuthorizationUrl():string ;
+    abstract exchangeCodeForToken(code: string): Promise<TokenResponse> ;
+    abstract getUserProfile(): Promise<UserProfile>
+}
