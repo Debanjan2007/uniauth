16b29839946c53b809fa1f92aeafc859026a198f
Debanjan Das <microsoft2420@gmail.com>
2026-05-12 23:29:23 +0530

    Add way to use the memory to store pkce codes

---
+++ src/pkce/memory.types.ts
@@ -0,0 +1,5 @@
+﻿export type useMemoryType = {
+    key: string , // unique key
+    value: string,
+    ttl: number  // in miliseconds
+}
---
+++ src/pkce/sessionManagement.ts
@@ -0,0 +1,11 @@
+﻿import type { useMemoryType } from "./memory.types"
+const memorymanager = new Map()
+
+export function useMemory({key , value , ttl} : useMemoryType){
+    const expiresAt = Date.now() + ttl
+    memorymanager.set(key , {value , expiresAt})
+
+    setTimeout(() => {
+        memorymanager.delete(key)
+    }, ttl)
+}
