f1d778b0bae5a421dc93ae080ae36ff3c251cc9d
Debanjan Das <microsoft2420@gmail.com>
2026-05-14 00:36:31 +0530

    Baseclass relocation

---
+++ src/providers/BaseClass.ts
@@ -0,0 +1,8 @@
+﻿import type { TokenResponse } from "./core/types/TokenResponse.types";
+import type { UserProfile } from "./core/types/UserProfile.types";
+
+export abstract class BaseProviderClass{
+    abstract getAuthorizationUrl():string ;
+    abstract exchangeCodeForToken(code: string): Promise<TokenResponse> ;
+    abstract getUserProfile(accessToken: string): Promise<UserProfile>
+}
