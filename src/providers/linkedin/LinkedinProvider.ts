916265b3378ba98bbbae633bf1a600fce5b72ca5
Debanjan Das <microsoft2420@gmail.com>
2026-05-14 00:21:10 +0530

    Add linkedin Provider with every required functions

---
+++ src/providers/linkedin/LinkedinProvider.ts
@@ -0,0 +1,91 @@
+﻿import axios from 'axios'
+import { BaseProviderClass } from '../core/BaseClass'
+import { TokenResponse } from '../core/types/TokenResponse.types';
+import { UserProfile } from '../core/types/UserProfile.types';
+import { generateCodeVerifier as generateState } from '../../pkce/generateCodeVerifier'
+import { LinkedinConstants } from './linkedin.constants'
+import type { LinkedinAuthParams } from './Linkedin.types'
+
+export class LinkedinProvider extends BaseProviderClass {
+    private clientId: string
+    private clientSecret: string
+    private redirectUrl: string
+    private scope: string[]
+
+
+    constructor({ clientId, clientSecret, redirecturl, scope }: LinkedinAuthParams) {
+        super()
+        this.clientId = clientId
+        this.scope = scope
+        this.clientSecret = clientSecret
+        this.redirectUrl = redirecturl
+    }
+    async exchangeCodeForToken(code: string): Promise<TokenResponse> {
+        try {
+            const url = LinkedinConstants.AccessTokenUrl + new URLSearchParams({
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: this.redirectUrl,
+                client_id: this.clientId,
+                client_secret: this.clientSecret,
+            })
+
+            const { data } = await axios.post<{ access_token: string; expires_in: number; refresh_token?: string; scope?: string; token_type: string; id_token?: string }>(
+                url,
+                undefined,
+                {
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    }
+                }
+            )
+            const { access_token, expires_in, refresh_token, scope, token_type, id_token } = data
+            const response: TokenResponse = {
+                accessToken: access_token,
+                refreshToken: refresh_token,
+                expiresIn: expires_in,
+                idToken: id_token,
+                scope: scope,
+                tokenType: token_type
+            }
+
+            return response
+        } catch (error) {
+            console.log(error);
+            throw new Error("Error occured exchanging code for the access token", { cause: error })
+        }
+    }
+    getAuthorizationUrl(): string {
+        const state: string = generateState()
+        const scope = this.scope.join("%20")
+        const url = LinkedinConstants.AuthUrl + new URLSearchParams({
+            response_type: "code",
+            client_id: this.clientId,
+            redirect_ur: encodeURIComponent(this.redirectUrl),
+            state: state,
+            scope: scope
+        })
+
+        return url
+    }
+    async getUserProfile(accessToken: string): Promise<UserProfile> {
+        const { data } = await axios.get<{
+            email?: string;
+            name?: string;
+            picture?: string;
+        }>(LinkedinConstants.UserProfile, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+        const { picture, name, email } = data
+        const user: UserProfile = {
+            provider: 'Linkedin',
+            avatarUrl: picture,
+            displayName: name,
+            email: email,
+            raw: data
+        }
+        return user
+    }
+}
