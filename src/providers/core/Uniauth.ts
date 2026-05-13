27c8a708730a5c39edd6b4eb091daf06a4bc5edb
Debanjan Das <microsoft2420@gmail.com>
2026-05-14 01:04:44 +0530

    Add the core Uniauth provider

---
+++ src/providers/core/Uniauth.ts
@@ -0,0 +1,14 @@
+﻿import { UniauthConfig } from './types/UniauthConfig.types'
+import { LinkedinProvider }  from '../linkedin/LinkedinProvider'
+
+export class Uniauth {
+    private providers = new Map<string, any>
+    constructor(config: UniauthConfig) {
+        if(config.providers?.Linkedin){
+            this.providers.set(
+                'Linkedin',
+                new LinkedinProvider(config.providers.Linkedin)
+            )
+        }
+    }
+}
