df6a167acab0fcde81e07ade3370f055a1002391
Debanjan Das <microsoft2420@gmail.com>
2026-05-18 08:59:07 +0530

    Update constants and the types for google

--- src/providers/google/Google.constants.ts
+++ src/providers/google/Google.constants.ts
@@ -1,4 +1,4 @@
-﻿export const LinkedinConstants = {
+﻿export const GoogleConstants = {
     AuthUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
     AccessTokenUrl: 'https://oauth2.googleapis.com/token',
 }
