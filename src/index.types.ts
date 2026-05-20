274a8add307f019c42ff0af3dbfb1478bafa506b
Debanjan Das <microsoft2420@gmail.com>
2026-05-20 23:49:30 +0530

    Exporting all types

---
+++ src/index.types.ts
@@ -0,0 +1,12 @@
+﻿/**
+ * Barrel export for shared public type definitions used by Uniauth consumers.
+ *
+ * This file re-exports provider-specific and core SDK types in a single import
+ * surface for convenience.
+ */
+export * from './providers/core/types/TokenResponse.types.js'
+export * from './providers/core/types/UniauthConfig.types.js'
+export * from './providers/core/types/UserProfile.types.js'
+export * from './providers/google/Google.types.js'
+export * from './providers/linkedin/Linkedin.types.js'
+export * from './pkce/memory.types.js'
