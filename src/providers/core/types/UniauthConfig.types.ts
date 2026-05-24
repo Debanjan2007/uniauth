e35bd8b656a490749332ce2f7dc0e4edb182ca5e
Debanjan Das <microsoft2420@gmail.com>
2026-05-24 10:19:41 +0530

    Updated core class type to match the import

--- src/providers/core/types/UniauthConfig.types.ts
+++ src/providers/core/types/UniauthConfig.types.ts
@@ -1,12 +1,12 @@
-﻿import type { LinkedinAuthParams } from '../../linkedin/Linkedin.types.js'
-import type { GoogleAuthParams } from '../../google/Google.types.js'
+﻿import type { AuthParams } from '../Shared/AuthParams.types.js'
 
 /**
  * Configuration object for initializing the Uniauth client.
  */
 export interface UniauthConfig {
     providers : {
-        Linkedin?: LinkedinAuthParams
-        Google?: GoogleAuthParams
+        Linkedin?: AuthParams
+        Google?: AuthParams
+        Github?: AuthParams
     }
 }
