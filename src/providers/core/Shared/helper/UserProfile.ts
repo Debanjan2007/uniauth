adb887633203f6d688fc9ba07a4590c136266f44
Debanjan Das <microsoft2420@gmail.com>
2026-05-24 10:11:41 +0530

    Add getUserProfile shared function

---
+++ src/providers/core/Shared/helper/UserProfile.ts
@@ -0,0 +1,32 @@
+﻿import axios from "axios";
+import type { UserProfile } from "../../types/UserProfile.types.js";
+
+const getUserProfile = async(accessToken: string , UserProfileUrl: string , provider: 'Google' | 'Likedin' | 'Github'): Promise<UserProfile> => {
+    try {
+            const { data } = await axios.get<{
+            email?: string;
+            name?: string;
+            picture?: string;
+        }>(
+            UserProfileUrl , {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+        const { picture, name, email } = data
+        const user: UserProfile = {
+            provider,
+            avatarUrl: picture,
+            displayName: name,
+            email: email,
+            raw: data
+        }
+        return user
+        } catch (error) {
+            throw new Error(`Something went wrong while fetching the user details from ${provider}, try again later.`, {cause: error})
+        }
+}
+
+export {
+    getUserProfile
+}
