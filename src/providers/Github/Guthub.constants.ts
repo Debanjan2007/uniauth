f55760044a00dd24c638cf193259a94eb6c3269a
Debanjan Das <microsoft2420@gmail.com>
2026-05-31 20:23:23 +0530

    Update Github constants

--- src/providers/Github/Guthub.constants.ts
+++ src/providers/Github/Guthub.constants.ts
@@ -1,3 +1,5 @@
 ﻿export const GithubConstants = {
-    Endpoint : 'https://github.com'
+    AuthUrl : 'https://github.com/login/oauth/authorize',
+    AccessTokenUrl: 'https://github.com/login/oauth/access_token',
+    UserProfile: 'https://api.github.com/user'
 }
