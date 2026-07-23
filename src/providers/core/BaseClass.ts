923e281e65e4e100ce2b2a012c9a185710f7acb1
Debanjan Das <microsoft2420@gmail.com>
2026-07-23 10:08:23 +0530

    Add optional refreshAccessToken method to BaseProviderClass

--- src/providers/core/BaseClass.ts
+++ src/providers/core/BaseClass.ts
@@ -4,5 +4,6 @@ import type { UserProfile } from "./types/UserProfile.types.js";
 export abstract class BaseProviderClass{
     abstract getAuthorizationUrl():string ;
     abstract exchangeCodeForToken(code: string): Promise<TokenResponse> ;
-    abstract getUserProfile(accessToken: string): Promise<UserProfile>
+    abstract getUserProfile(accessToken: string): Promise<UserProfile> ;
+    refreshAccessToken?(refreshToken: string): Promise<unknown> ;
 }
