86ba613b308f9fe550cfc424cb0bbd84da2c79eb
Debanjan Das <microsoft2420@gmail.com>
2026-08-12 22:23:57 +0530

    Add InstagramProvider and constants for Instagram authentication

---
+++ src/providers/instagram/Instagram.constants.ts
@@ -0,0 +1,6 @@
+﻿export const InstagramConstants = {
+    AuthUrl: 'https://api.instagram.com/oauth/authorize',
+    AccessTokenUrl: 'https://api.instagram.com/oauth/access_token',
+    RefreshTokenUrl: 'https://graph.instagram.com/refresh_access_token',
+    UserProfile: 'https://graph.instagram.com/me'
+}
